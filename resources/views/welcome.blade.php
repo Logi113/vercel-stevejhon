3a20934acefafab03fa4f538a8600be9958f7a15
Your Name <you@example.com>
2024-05-15 04:50:15 +0800

    1st commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Portfolio</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- w3-content defines a container for fixed size centered content, 
+and is wrapped around the whole page content, except for the footer in this example -->
+<div class="w3-content" style="max-width:1400px">
+
+<!-- Header -->
+<header class="w3-container w3-center w3-padding-32"> 
+  <h1><b>MY PORTFOLIO</b></h1>
+  <p>Welcome to my Portfolio <span class="w3-tag">Steve Jhon Delmendo</span></p>
+</header>
+
+<!-- Grid -->
+<div class="w3-row">
+
+<!-- Blog entries -->
+<div class="w3-col l8 s12">
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+    <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/440865949_884219776757175_8936368028385228484_n.jpg?stp=dst-jpg_p320x320&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEGBm2OuvT_efctbGHNkp8iuWJH47qmjra5YkfjuqaOtuta093CmJySqODQmWZ0_NpmMthTjZdv5Q7vAM0mUSk3&_nc_ohc=ko1q6RwgRzIQ7kNvgGtqIJs&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QEBrEa0GEXn4429_buONbDEYNWJ5hJB9CWSR8k-3nJhtw&oe=666AF91B" alt="img" style="width:70%">
+    <div class="w3-container">
+      <h3><b>My Interest</b></h3>
+      
+    </div>
+
+    <div class="w3-container">
+      <p>Apart from enjoying the tech stuff, i enjoy watching anime, and hanging out with my family, for me theres a unique joy hanging out with my family.
+        aside from those i also like hanging out at the beach and watching the sunset i also like discovering new flavors in food</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+          <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+        </div>
+        <div class="w3-col m4 w3-hide-small">
+          <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-tag">0</span></span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr>
+
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="https://honestproscons.com/wp-content/uploads/2021/02/computer-technology.jpg" alt="img" style="width:100%">
+    <div class="w3-container">
+      <h3><b>Skills</b></h3>
+    </div>
+
+    <div class="w3-container">
+      <p>While i said i am studying in IT im not particulary good at those but one skill i atleast have some confidence
+        is critcal thinking and analyzing skills although my skills are not related to a popular skill set which is programming i can say that 
+        having analyzing skills which is also important in solvingh problems is as important as a coding skills
+      </p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+          <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+        </div>
+        <div class="w3-col m4 w3-hide-small">
+          <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-badge">2</span></span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- END BLOG ENTRIES -->
+</div>
+
+<!-- Introduction menu -->
+<div class="w3-col l4">
+  <!-- About Card -->
+  <div class="w3-card w3-margin w3-margin-top">
+  <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/441551960_310217015456107_7346703158085680979_n.jpg?stp=dst-jpg_s843x403&_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFftxFdZGZSjTHS7hMJ7JGCmJ0tNQNJM0-YnS01A0kzT67kulFS58G5eFME68JctrHlCBf11L8YayV4_AQbucaV&_nc_ohc=RdOLcBaQOwQQ7kNvgHx5ySY&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QE0XVzfRqmB6_ECvRgdVahoXRLGp_3rmITwiJ25g04Hmw&oe=666B023B" alt="img" style="width:100%">
+    <div class="w3-container w3-white">
+      <h4><b>Steve Jhon B. Delmendo</b></h4>
+      <p>Just me, myself and I, exploring the universe of uknownment. I have a heart of love and a interest in technology which is why i pursued IT.</p>
+    </div>
+  </div><hr>
+  
+  <!-- Posts -->
+  <div class="w3-card w3-margin">
+    <div class="w3-container w3-padding">
+      <h4>Hobbies</h4>
+    </div>
+    <ul class="w3-ul w3-hoverable w3-white">
+      <li class="w3-padding-16">
+        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/441266806_434606762522680_2726334599899173647_n.jpg?stp=dst-jpg_s403x403&_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE3RWn4dfV5MjHY56NiKN-rpA89IWam_0SkDz0hZqb_RD5Yq_7MRs2_jDl7YnL7ljWlzEQ4z7vR8xJiSzNJZE7J&_nc_ohc=oes3-TtnmccQ7kNvgE_ghHD&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QEmbuZ2J8Sa2fVHOfR5uNVnh5wZCrW6iU71YK0wNAA3rg&oe=666B2261" alt="img" style="width:50px">
+        <span class="w3-large">Taking Pictures</span><br>
+      </li>
+      <li class="w3-padding-16">
+        <img src="https://img.freepik.com/free-photo/anime-style-character-space_23-2151134100.jpg" alt="img" style="width:60px">
+        <span class="w3-large">Animes</span><br>
+      </li> 
+      <li class="w3-padding-16">
+        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/440740940_890346596190584_8986867313624837065_n.jpg?stp=dst-jpg_s403x403&_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG6Q3F6ZhS8_X9NoCCp3tryA9xtESt7VDgD3G0RK3tUOGknOTfAhNN3QzgxtLPxErf5ucGjNR2dHKM7LUvGLhAt&_nc_ohc=3sTyQq1hOM8Q7kNvgHeHdZX&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QGYOOGLUblIXJKZlU_RJi_Es3zkmEEFOoeDiz4xaNRlew&oe=666B18A1"  alt="img" style="width:60px">
+        <span class="w3-large">Eating</span><br>
+      </li>   
+    </ul>
+  </div>
+  <hr> 
+ 
+  <!-- Labels / tags -->
+  <div class="w3-card w3-margin">
+    <div class="w3-container w3-padding">
+      <h4>Tags</h4>
+    </div>
+    <div class="w3-container w3-white">
+    <p><span class="w3-tag w3-black w3-margin-bottom">Travel</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">New York</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">London</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">IKEA</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">NORWAY</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">DIY</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Ideas</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Baby</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Family</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">News</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Clothing</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Shopping</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Sports</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Games</span>
+    </p>
+    </div>
+  </div>
+  
+<!-- END Introduction Menu -->
+</div>
+
+<!-- END GRID -->
+</div><br>
+
+<!-- END w3-content -->
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
+  <button class="w3-button w3-black w3-disabled w3-padding-large w3-margin-bottom">Previous</button>
+  <button class="w3-button w3-black w3-padding-large w3-margin-bottom">Next »</button>
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>
+
+</body>
+</html>
+<!DOCTYPE html>
+<html>
+<head>
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- w3-content defines a container for fixed size centered content, 
+and is wrapped around the whole page content, except for the footer in this example -->
+<div class="w3-content" style="max-width:1400px">
+
+<!-- Header -->
+<header class="w3-container w3-center w3-padding-32"> 
+  <h1><b>MY BLOG</b></h1>
+  <p>Welcome to my Portfolio <span class="w3-tag">Steve Jhon Delmendo</span></p>
+</header>
+
+<!-- Grid -->
+<div class="w3-row">
+
+<!-- Blog entries -->
+<div class="w3-col l8 s12">
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+    <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/440865949_884219776757175_8936368028385228484_n.jpg?stp=dst-jpg_p320x320&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEGBm2OuvT_efctbGHNkp8iuWJH47qmjra5YkfjuqaOtuta093CmJySqODQmWZ0_NpmMthTjZdv5Q7vAM0mUSk3&_nc_ohc=ko1q6RwgRzIQ7kNvgGtqIJs&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QEBrEa0GEXn4429_buONbDEYNWJ5hJB9CWSR8k-3nJhtw&oe=666AF91B" alt="img" style="width:70%">
+    <div class="w3-container">
+      <h3><b>My Interest</b></h3>
+      
+    </div>
+
+    <div class="w3-container">
+      <p>Apart from enjoying the tech stuff, i enjoy watching anime, and hanging out with my family, for me theres a unique joy hanging out with my family.
+        aside from those i also like hanging out at the beach and watching the sunset i also like discovering new flavors in food</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+          <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+        </div>
+        <div class="w3-col m4 w3-hide-small">
+          <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-tag">0</span></span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr>
+
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="https://honestproscons.com/wp-content/uploads/2021/02/computer-technology.jpg" alt="img" style="width:100%">
+    <div class="w3-container">
+      <h3><b>Skills</b></h3>
+    </div>
+
+    <div class="w3-container">
+      <p>While i said i am studying in IT im not particulary good at those but one skill i atleast have some confidence
+        is critcal thinking and analyzing skills although my skills are not related to a popular skill set which is programming i can say that 
+        having analyzing skills which is also important in solvingh problems is as important as a coding skills
+      </p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+          <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+        </div>
+        <div class="w3-col m4 w3-hide-small">
+          <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-badge">2</span></span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- END BLOG ENTRIES -->
+</div>
+
+<!-- Introduction menu -->
+<div class="w3-col l4">
+  <!-- About Card -->
+  <div class="w3-card w3-margin w3-margin-top">
+  <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/441551960_310217015456107_7346703158085680979_n.jpg?stp=dst-jpg_s843x403&_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFftxFdZGZSjTHS7hMJ7JGCmJ0tNQNJM0-YnS01A0kzT67kulFS58G5eFME68JctrHlCBf11L8YayV4_AQbucaV&_nc_ohc=RdOLcBaQOwQQ7kNvgHx5ySY&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QE0XVzfRqmB6_ECvRgdVahoXRLGp_3rmITwiJ25g04Hmw&oe=666B023B" alt="img" style="width:100%">
+    <div class="w3-container w3-white">
+      <h4><b>Steve Jhon B. Delmendo</b></h4>
+      <p>Just me, myself and I, exploring the universe of uknownment. I have a heart of love and a interest in technology which is why i pursued IT.</p>
+    </div>
+  </div><hr>
+  
+  <!-- Posts -->
+  <div class="w3-card w3-margin">
+    <div class="w3-container w3-padding">
+      <h4>Hobbies</h4>
+    </div>
+    <ul class="w3-ul w3-hoverable w3-white">
+      <li class="w3-padding-16">
+        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/441266806_434606762522680_2726334599899173647_n.jpg?stp=dst-jpg_s403x403&_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE3RWn4dfV5MjHY56NiKN-rpA89IWam_0SkDz0hZqb_RD5Yq_7MRs2_jDl7YnL7ljWlzEQ4z7vR8xJiSzNJZE7J&_nc_ohc=oes3-TtnmccQ7kNvgE_ghHD&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QEmbuZ2J8Sa2fVHOfR5uNVnh5wZCrW6iU71YK0wNAA3rg&oe=666B2261" alt="img" style="width:50px">
+        <span class="w3-large">Taking Pictures</span><br>
+      </li>
+      <li class="w3-padding-16">
+        <img src="https://img.freepik.com/free-photo/anime-style-character-space_23-2151134100.jpg" alt="img" style="width:60px">
+        <span class="w3-large">Animes</span><br>
+      </li> 
+      <li class="w3-padding-16">
+        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/440740940_890346596190584_8986867313624837065_n.jpg?stp=dst-jpg_s403x403&_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG6Q3F6ZhS8_X9NoCCp3tryA9xtESt7VDgD3G0RK3tUOGknOTfAhNN3QzgxtLPxErf5ucGjNR2dHKM7LUvGLhAt&_nc_ohc=3sTyQq1hOM8Q7kNvgHeHdZX&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QGYOOGLUblIXJKZlU_RJi_Es3zkmEEFOoeDiz4xaNRlew&oe=666B18A1"  alt="img" style="width:60px">
+        <span class="w3-large">Eating</span><br>
+      </li>   
+    </ul>
+  </div>
+  <hr> 
+ 
+  <!-- Labels / tags -->
+  <div class="w3-card w3-margin">
+    <div class="w3-container w3-padding">
+      <h4>Tags</h4>
+    </div>
+    <div class="w3-container w3-white">
+    <p><span class="w3-tag w3-black w3-margin-bottom">Travel</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">New York</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">London</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">IKEA</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">NORWAY</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">DIY</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Ideas</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Baby</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Family</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">News</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Clothing</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Shopping</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Sports</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Games</span>
+    </p>
+    </div>
+  </div>
+  
+<!-- END Introduction Menu -->
+</div>
+
+<!-- END GRID -->
+</div><br>
+
+<!-- END w3-content -->
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
+  <button class="w3-button w3-black w3-disabled w3-padding-large w3-margin-bottom">Previous</button>
+  <button class="w3-button w3-black w3-padding-large w3-margin-bottom">Next »</button>
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>
+
+</body>
+</html>
